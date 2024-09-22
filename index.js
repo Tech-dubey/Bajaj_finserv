e90e677c9c662e754314fd995b33671910e4c06d
Tech-dubey <91800789+Tech-dubey@users.noreply.github.com>
2024-09-22 18:38:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,5 +108,5 @@ app.post("/bfhl", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
